1d2755fced4886132390d1023459414ac27da006
chriscphillips <cptp50wv@shentel.net>
2018-01-26 07:26:01 -0500

    update

--- lotto_number.rb
+++ lotto_number.rb
@@ -4,18 +4,11 @@
 
 	def winning_numbers()
 		$winning_numbers = $number_array.sample(6).sort
+		$winning_numbers
 	end
 
-def winning_numbers(available_numbers)
-    winning_numbers = ()
-    available_numbers = (1..99)
-    winning_numbers = available_numbers    
-	winning_numbers.each do |number, numbers|
-    if winning_numbers >= 0
-    numbers_returned[numbers] = winning_numbers 
-    i = numbers_returned[numbers] 
-    numbers = numbers - i
-    end
-    winning_numbers      
-end
-end
+
+lotto()
+winning_numbers()
+	 
+
